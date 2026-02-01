7f5728746320216edc5f68938e095c299eec6e01
David Adakole <ccrowndavid@gmail.com>
2026-02-01 12:18:14 +0100

    Changed font from titillium-web to geist

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,19 @@
 import type { Metadata } from "next";
-import { Titillium_Web } from "next/font/google";
+import { Titillium_Web, Geist, Geist_Mono } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
 import "./globals.css";
+
+const geist_mono = Geist_Mono({
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "600", "700", "900"],
+  variable: "--font-geist-mono",
+});
+
+const geist = Geist({
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "600", "700", "900"],
+  variable: "--font-geist",
+});
 
 const titillium = Titillium_Web({
   subsets: ["latin"],
@@ -21,7 +33,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${titillium.variable} font-sans antialiased`}>
+      <body className={`${titillium.variable} ${geist.variable} ${geist_mono.variable} font-sans antialiased`}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
